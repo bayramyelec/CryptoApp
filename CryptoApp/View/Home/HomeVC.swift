72e28e9f718a06b901dfefc191da7b6f95268272
Bayram Yelec <bayrammyelec@gmail.com>
2025-01-31 11:28:51 +0300

    commit

--- CryptoApp/View/Home/HomeVC.swift
+++ CryptoApp/View/Home/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  CryptoApp
 //
 //  Created by Bayram Yeleç on 31.01.2025.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
 }
-
